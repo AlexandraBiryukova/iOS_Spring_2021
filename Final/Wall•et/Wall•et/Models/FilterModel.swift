dcd99b71af18289f60babcee1d60e01aed05c0ba
Alexandra <alexbirukova@mail.ru>
2021-05-10 19:21:53 +0600

    fix: правки

---
+++ Final/Wall•et/Wall•et/Models/FilterModel.swift
@@ -0,0 +1,50 @@
+//
+//  FilterModel.swift
+//  Wall•et
+//
+//  Created by Alexandra Biryukova on 5/10/21.
+//
+
+import Foundation
+
+enum TransactionsCount: String, Codable, PickerItem, CaseIterable {
+    case none
+    case zero = "0"
+    case between0and10 = "1 - 10"
+    case between10and25 = "11 - 25"
+    case moreThan25 = "25>"
+    
+    var title: String {
+        switch self {
+        case .none:
+            return "Любое"
+        default:
+            return self.rawValue
+        }
+    }
+}
+
+enum FilterCategory: String, Codable, PickerItem, CaseIterable {
+    case products
+    case clothes
+    
+    var title: String {
+        switch self {
+        case .none:
+            return "Все"
+        case .products:
+            return "Еда и продукты"
+        case .clothes:
+            return "Одежда и обувь"
+        }
+    }
+}
+
+struct FilterModel {
+    var count: TransactionsCount = .none
+    var category: FilterCategory = .none
+    var onlyFavourites = false
+    var usingPeriod = false
+    var startDate = Date()
+    var endDate = Date()
+}
--- Final/Wall•et/Wall•et/Screens/Places/Views/FilterView.swift
+++ Final/Wall•et/Wall•et/Screens/Places/Views/FilterView.swift
@@ -7,49 +7,6 @@
 
 import SwiftUI
 
-enum TransactionsCount: String, Codable, PickerItem, CaseIterable {
-    case none
-    case zero = "0"
-    case between0and10 = "1 - 10"
-    case between10and25 = "11 - 25"
-    case moreThan25 = "25>"
-    
-    var title: String {
-        switch self {
-        case .none:
-            return "Любое"
-        default:
-            return self.rawValue
-        }
-    }
-}
-
-enum FilterCategory: String, Codable, PickerItem, CaseIterable {
-    case products
-    case clothes
-    case none
-    
-    var title: String {
-        switch self {
-        case .none:
-            return "Все"
-        case .products:
-            return "Еда и продукты"
-        case .clothes:
-            return "Одежда и обувь"
-        }
-    }
-}
-
-struct FilterModel {
-    var count: TransactionsCount = .none
-    var category: FilterCategory = .none
-    var onlyFavourites = false
-    var usingPeriod = false
-    var startDate = Date()
-    var endDate = Date()
-}
-
 struct FilterView: View {
     @State var filterModel: FilterModel
     @Binding var presentFilter: Bool
